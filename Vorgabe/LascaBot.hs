f6c719f76c27b5b80b0847bb13fb9ec1edf11c00
max kieltyka <maxkieltyka@gmail.com>
2017-02-15 04:53:39 +0100

    schönes parsing hinzugefügt

--- Vorgabe/LascaBot.hs
+++ Vorgabe/LascaBot.hs
@@ -6,109 +6,59 @@ import Data.Char
 import System.Environment
 --- external signatures (NICHT AENDERN!)
 getMove   :: String -> String
--- listMoves :: String -> String
+listMoves :: String -> String
 
 -- *==========================================* --
 -- |    HIER BEGINNT EURE IMPLEMENTIERUNG!    | --
 -- *==========================================* --
 
+
 --- types/structures (TODO)
 
 data Color = White | Black
 
-currentState = "b,b,b,b/b,b,b/b,b,,b/,b,/w,w,w,w/w,w,w/w,w,w,w w" :: String
--- showMoves = listMoves currentState :: String
-    --- ... ---
 
-listMoves :: String
-listMoves = parseInput (splitOn " " currentState)
-
-parseInput :: [String] -> String
-parseInput (board:color:[]) = (findMoves (splitOn "/" currentState) color)
-
-findMoves :: [String] -> String -> String
--- findMoves board "w" = head (splitOn "," (head board))
-findMoves board "w" = findAllJumps board "w" 1 1
-findMoves board "b" = "blacks turn"
-
-findAllJumps :: [String] -> String -> Integer -> Integer -> String
-findAllJumps board "w" 7 7 = if (isOwnedByWhite(getField board 7 7)) 
-							then addMoveToList
-							else "false"
-findAllJumps board "w" row column = if (isOwnedByWhite(getField board row column)) 
-							then findAllJumpsPerRow board "w" 6
-							else "false"
-
-findAllJumpsPerRow board "w" 7 = "row"
-findAllJumpsPerRow board "w" i = findAllJumpsPerRow board "w" (i+1)
-
-addMoveToList = "move"
-
-isOwnedByWhite :: String -> Bool
-isOwnedByWhite s
-			| (((head s) == 'w') || ((head s) == 'W')) = True
-isOwnedByWhite _ = False
-
-getField :: [String] -> Integer -> Integer -> String
-getField (a:b:c:d:e:f:g:[]) 1 column = getElementOdd (splitOn "," g) column
-getField (a:b:c:d:e:f:g:[]) 2 column = getElementEven (splitOn "," f) column
-getField (a:b:c:d:e:f:g:[]) 3 column = getElementOdd (splitOn "," e) column
-getField (a:b:c:d:e:f:g:[]) 4 column = getElementEven (splitOn "," d) column
-getField (a:b:c:d:e:f:g:[]) 5 column = getElementOdd (splitOn "," c) column
-getField (a:b:c:d:e:f:g:[]) 6 column = getElementEven (splitOn "," b) column
-getField (a:b:c:d:e:f:g:[]) 7 column = getElementOdd (splitOn "," a) column
-
-getElementEven :: [String] -> Integer -> String
-getElementEven (b:d:f:[]) 2 = b
-getElementEven (b:d:f:[]) 4 = d
-getElementEven (b:d:f:[]) 6 = f
-getElementEven _ _ = "x"
-
-getElementOdd :: [String] -> Integer -> String
-getElementOdd (a:c:e:g:[]) 1 = a
-getElementOdd (a:c:e:g:[]) 3 = c
-getElementOdd (a:c:e:g:[]) 5 = e
-getElementOdd (a:c:e:g:[]) 7 = g
-getElementOdd _ _ = "x"
-
-toString 1 = "1"
-toString 2 = "2"
-toString 3 = "3"
-toString 4 = "4"
-toString 5 = "5"
-toString 6 = "6"
-toString 7 = "7"
+--type Move = String -- noch einen besseren typ ausdenken!
 
 
+data Field = Field Char Int
+data Move = Move Field Field
 
 
---getRow :: [String] -> Integer -> String
---getRow board 7 = head board
---getRow (_:rest) (odd r) = getRow rest (succ r)
---getRow (_:rest) (even r) = getRow rest (succ r)
+type Row = [String]
 
---getElement row 1 = head row
---getElement (_:rest) e = getElement rest (pred e)
+type Board = [Row]
+
+
+data Game = Game Board Color Move |
+            Game2 Board Color 
+
+
 
 --- logic (TODO)
 getMove   s = "g3-f4" -- Eigene Definition einfügen!
--- listMoves s = "[g3-f4,...]" -- Eigene Definition einfügen!
---listMoves :: String -> [String]
---listMoves s = parseInput s
+listMoves s = "[g3-f4,...]" -- Eigene Definition einfügen!
 
---parseInput :: String -> [String]
---parseInput s = splitOn "/" s
-    --- ... ---
 
---- input (TODO)
 
--- parse :: String -> Color
--- parse :: String -> [String]
--- parse s = parseInput (splitOn " " s)
+--listMoves s = let
+--    g = parseInput s
 
--- parseInput :: [String] -> Color
--- parseInput (board:color:[])   = (parseColor color)
--- parseInput (board:color:move:[]) = ... (parseColor color) ...
+ --   in
+
+
+
+-- input and parsing
+
+-- spaltet an den leerzeichen
+parse :: String -> Game
+parse s = parseInput (splitOn " " s)
+
+-- sammelt die einzelteile und erstellt ein Game
+parseInput :: [String] -> Game
+parseInput (g:c:[]) = Game2  (parseBoard g) (parseColor c) 
+parseInput (g:c:m:[]) = Game (parseBoard g) (parseColor c) (parseMove m)
+
 
 parseColor :: String -> Color
 parseColor "w" = White
@@ -117,7 +67,30 @@ parseColor "b" = Black
 toInt :: Char -> Int
 toInt c = ((ord c) - (ord 'a') + 1)
 
-    --- ... ---
+
+-- erstellt aus einzelnen zeilen ein Board
+parseBoard :: String -> Board 
+parseBoard b = map parseRow (splitOn "/" b) 
+
+
+-- erstellt aus einzelnen feldern eine zeile
+parseRow :: String -> Row 
+parseRow r = splitOn "," r
+
+
+-- erstellt eine Liste von 2 Fields 
+-- gibt sie als Move zurück
+parseMove :: String -> Move
+parseMove s = parseFields (map parseField (splitOn "-" s))
+
+-- nimmt 2 Fields und packt sie in ein Move
+parseFields :: [Field] -> Move
+parseFields (f:g:[]) = Move f g 
+
+-- aus einem string ein field
+parseField :: String -> Field
+parseField (s:t:[]) = Field s (toInt t)
+
 
 --- output (TODO)
 
